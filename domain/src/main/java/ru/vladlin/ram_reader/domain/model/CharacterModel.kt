f696f5bb0bcf0f0eb97f581ce5bca80119c55658
Vladlin Moiseenko <mvladlin@gmail.com>
2022-05-06 10:16:47 +0300

    3. edit FragmentDetail

--- domain/src/main/java/ru/vladlin/ram_reader/domain/model/CharacterModel.kt
+++ domain/src/main/java/ru/vladlin/ram_reader/domain/model/CharacterModel.kt
@@ -13,19 +13,5 @@ data class CharacterModel(
     @Json(name = "gender") val gender: String?,
     @Json(name = "created") val created: String?,
     @Json(name = "image") val image: String?,
-    @Json(name = "species") val species: String?,
-
-    //@Json(name = "episode") val episode: List<Any>,
-    //@Json(name = "location") val location: Location,
-    //@Json(name = "origin") val origin: Origin
-)
-
-data class Location(
-    @Json(name = "name") val name: String,
-    @Json(name = "url") val url: String
-)
-
-data class Origin(
-    @Json(name = "name") val name: String,
-    @Json(name = "url") val url: String
+    @Json(name = "species") val species: String?
 )
